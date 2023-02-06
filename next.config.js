082a0dc72e1a83b41116baf8e5c4801aab80e935
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2023-02-06 08:50:59 +0400

    fixed alias SVGAtributes

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
 	webpack(config, options) {
 		config.module.rules.push({
 			loader: '@svgr/webpack',
+			issuer:/\.[jt]sx?$/,
 			options: {
 				prettier: false,
 				svgo: true,
