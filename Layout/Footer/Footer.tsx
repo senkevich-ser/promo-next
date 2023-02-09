dfa20b866dd9abea5b7c43d757d3f2048552fe94
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2023-02-09 11:49:49 +0400

    added footer adaptive

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,15 +1,18 @@
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
 import cn from "classnames";
+import {format} from 'date-fns';
 
 export const Footer = ({
-  ...props
+  className,...props
 }: FooterProps): JSX.Element => {
   return (
-      <div
+      <footer className={cn(styles.footer,className)}
         {...props}
       >
-        Footer
-    </div>
+        <div>OwlTop © 2020 - {format(new Date(),'yyyy')} Все права защищены</div>
+        <a href='#' target={"_blank"}>Пользовательское соглашение</a>
+        <a href='#' target={"_blank"}>Политика конфиденциальности</a>
+    </footer>
   );
 };
