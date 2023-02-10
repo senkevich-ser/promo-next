29fddbaf1d82ef414fe9c502c78b371063d842a0
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2023-02-10 11:32:45 +0400

    added menu firsCategory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,65 @@
-import { Htag, Paragraph,Button,Tag,Rating } from "@/components";
+import { Htag, Paragraph, Button, Tag, Rating } from "@/components";
 import { withLayout } from "@/Layout/Layout";
 import { useState } from "react";
+import axios from "axios";
+import { GetStaticProps } from "next";
+import { MenuItem } from "@/interfaces/menu.interface";
 
-function Home(): JSX.Element {
-
-  const[rating,setRating]=useState(1);
+function Home({ menu }: HomeProps): JSX.Element {
+  const [rating, setRating] = useState(1);
   return (
     <>
-      <Htag tag='h1'>Text</Htag>
-      <Button appearance="primary"arrow="right">Button</Button>
-      <Button appearance="ghost" arrow="down">Button</Button>
-      <Paragraph size='l'>{'Большой'}</Paragraph>
-      <Paragraph size={'m'}>{'Средний'}</Paragraph>
-      <Paragraph size={'s'}>{'Маленький'}</Paragraph>
-      <Tag size={'l'} color={'grey'}>Кнопка</Tag>
-      <Tag size={'s'} color={'ghost'}>Кнопка</Tag>
-      <Tag size={'s'} color={'green'}>Кнопка</Tag>
-      <Tag size={'l'} color={'red'}>Кнопка</Tag>
-      <Tag size={'s'} color={'primary'}>Кнопка</Tag>
-      <Rating rating={rating} isEditable setRating={setRating}/>
+      <Htag tag="h1">Text</Htag>
+      <Button appearance="primary" arrow="right">
+        Button
+      </Button>
+      <Button appearance="ghost" arrow="down">
+        Button
+      </Button>
+      <Paragraph size="l">{"Большой"}</Paragraph>
+      <Paragraph size={"m"}>{"Средний"}</Paragraph>
+      <Paragraph size={"s"}>{"Маленький"}</Paragraph>
+      <Tag size={"l"} color={"grey"}>
+        Кнопка
+      </Tag>
+      <Tag size={"s"} color={"ghost"}>
+        Кнопка
+      </Tag>
+      <Tag size={"s"} color={"green"}>
+        Кнопка
+      </Tag>
+      <Tag size={"l"} color={"red"}>
+        Кнопка
+      </Tag>
+      <Tag size={"s"} color={"primary"}>
+        Кнопка
+      </Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
-};
-
+}
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",{firstCategory}
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
