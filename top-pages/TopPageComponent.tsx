a220b75b614ba8ccb0adb4b7e421a3383a9dd331
Siarhey <accc75-mail.ru@yandex.ru>
2023-04-05 12:09:59 +0400

    added functional & fixed styles for Sort element

--- top-pages/TopPageComponent.tsx
+++ top-pages/TopPageComponent.tsx
@@ -2,37 +2,64 @@ import { Advantages, HhData, Htag, Sort, Tag } from "@/components";
 import Head from "next/head";
 import styles from "./TopPageComponent.module.css";
 import { TopPageComponentProps } from "./TopPageComponent.props";
-import cn from 'classnames';
+import cn from "classnames";
 import { TopLevelCategory } from "@/interfaces/page.interface";
 import { SortEnum } from "@/components/Sort/Sort.props";
+import { useReducer } from "react";
+import { sortReducer } from "./sort.reducer";
 
 export const TopPageComponent = ({
   firstCategory,
   page,
   products,
 }: TopPageComponentProps): JSX.Element => {
+
+  const [{ products: sortedProducts, sort }, dispathSort] = useReducer(
+    sortReducer,
+    { products, sort: SortEnum.Rating }
+  );
+
+  const setSort = (sort:SortEnum)=>{
+    dispathSort({type:sort});
+  };
+
   return (
     <div className={styles.wrapper}>
-     <Head>
+      <Head>
         <title>{page.title}</title>
       </Head>
       <div className={cn(styles.title)}>
-      <Htag tag={"h1"}>{page.title}</Htag>
-      <Tag color="grey" size="l">{products.length}</Tag>
-      <Sort sort={SortEnum.Rating} setSort={()=>{}}/>
+        <Htag tag={"h1"}>{page.title}</Htag>
+        <Tag color="grey" size="l">
+          {products.length}
+        </Tag>
+        <Sort sort={sort} setSort={setSort} />
       </div>
-      <div className={styles.products}>{products && products.map((p) => <div key={p._id}>{p.title}</div>)}</div>
-      <div className={cn(styles.title,styles.hhTitle)}>
-      <Htag tag={"h2"}>Вакансии - {page.category}</Htag>
-      <Tag color="red" size="l">hh.ru</Tag>
+      <div className={styles.products}>
+        {sortedProducts && sortedProducts.map((p) => <div key={p._id}>{p.title}</div>)}
       </div>
-      {firstCategory==TopLevelCategory.Courses && page.hh && <HhData {...page.hh}/>}
+      <div className={cn(styles.title, styles.hhTitle)}>
+        <Htag tag={"h2"}>Вакансии - {page.category}</Htag>
+        <Tag color="red" size="l">
+          hh.ru
+        </Tag>
+      </div>
+      {firstCategory == TopLevelCategory.Courses && page.hh && (
+        <HhData {...page.hh} />
+      )}
       <Htag tag={"h2"}>Преимущества</Htag>
-      {page.advantages && <Advantages advantages={page.advantages}/>}
-      <div className={styles.seoText} dangerouslySetInnerHTML={{__html:page.seoText}}></div>
+      {page.advantages && <Advantages advantages={page.advantages} />}
+      <div
+        className={styles.seoText}
+        dangerouslySetInnerHTML={{ __html: page.seoText }}
+      ></div>
       <Htag tag={"h2"}>Получаемые навыки</Htag>
-      {page.tags && page.tags.map(t=><Tag color={'primary'} key={t}>{t}</Tag>)}
+      {page.tags &&
+        page.tags.map((t) => (
+          <Tag color={"primary"} key={t}>
+            {t}
+          </Tag>
+        ))}
     </div>
-    
   );
 };
