6d1f171f695aefcdac74fe9c97b15d7cea3fb900
senkevich-ser <72697195+senkevich-ser@users.noreply.github.com>
2023-05-07 18:55:05 +0400

    added react-hook-form

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -5,35 +5,51 @@ import { Input } from "../Input/Input";
 import { Rating } from "../Rating/Rating";
 import { Textarea } from "../Textarea/Textarea";
 import { Button } from "../Button/Button";
-import CloseIcon from './close.svg';
+import CloseIcon from "./close.svg";
+import { IReviewForm } from "./ReviewForm.interface";
+import { Controller, useForm } from "react-hook-form";
 
 export const ReviewForm = ({
   productId,
   className,
   ...props
 }: ReviewFormProps): JSX.Element => {
+  const { register, control, handleSubmit } = useForm<IReviewForm>();
+
+  const onSubmit = (data: IReviewForm) => {
+    console.log(data);
+  };
   return (
-    <form>
-    <div className={cn(styles.reviewForm, className)} {...props}>
-      <Input />
-      <Input className={styles.title} />
-      <div className={styles.rating}>
-        <span>Оценка:</span>
-        <Rating rating={2} />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={cn(styles.reviewForm, className)} {...props}>
+        <Input {...register('name')}/>
+        <Input {...register('title')} className={styles.title} />
+        <div className={styles.rating}>
+          <span>Оценка:</span>
+          <Controller
+          control={control}
+          name='rating'
+          render={({field})=>(
+            <Rating rating={field.value} isEditable setRating={field.onChange}/>
+          )}
+          />
+        </div>
+        <Textarea {...register('description')} className={styles.description} />
+        <div className={styles.submit}>
+          <Button appearance="primary">Отправить</Button>
+          <span className={styles.disclamer}>
+            * Перед публикацией отзыв пройдет предварительную модерацию и
+            проверку
+          </span>
+        </div>
       </div>
-      <Textarea className={styles.description}/>
-      <div className={styles.submit}>
-        <Button appearance="primary">Отправить</Button>
-        <span className={styles.disclamer}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
+      <div className={styles.success}>
+        <div className={styles.successtitle}>Ваш отзыв отправлен</div>
+        <div className={styles.successinfo}>
+          Спасибо, Ваш отзыв будет опубликован после проверки.
+        </div>
+        <CloseIcon className={styles.successicon} />
       </div>
-    </div>
-     <div className={styles.success}>
-     <div className={styles.successtitle}>Ваш отзыв отправлен</div>
-     <div className={styles.successinfo}>
-       Спасибо, Ваш отзыв будет опубликован после проверки.
-     </div>
-     <CloseIcon className={styles.successicon} />
-   </div>
-   </form>
+    </form>
   );
 };
